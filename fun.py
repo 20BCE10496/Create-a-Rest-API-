a3caa781e54da2d2dda2916f4d78f2819ef6b4c8
20BCE10496 <amishagoyal2805@gmail.com>
2023-01-25 00:26:45 +0530

    update

--- fun.py
+++ fun.py
@@ -4,18 +4,6 @@ def sum(a,b):
 def avg(a,b):
     return (a+b)/2
 
-def arm(n):
-    n=int(input("enter a no."))
-sum=0
-order=len(str(n))
-copy_n=n
-while(n>0):
-    digit=n%10
-    sum+=digit**order
-    n=n//10
-if(sum==copy_n):
-    print(f"{copy_n} is an armstrong no.")
-else:
-    print(f"{copy_n} is not an armstrong no.")        
+       
 
 
--- main.py
+++ main.py
@@ -5,5 +5,22 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+def arm(n):  
+
+    sum=0
+    order=len(str(n))
+    copy_n=n
+    while(n>0):
+        digit=n%10
+        sum+=digit**order
+        n=n//10
+    if(sum==copy_n):
+        print(f"{copy_n} is an armstrong no.")
+        return True
+    
+    else:
+       print(f"{copy_n} is not an armstrong no.")        
+       return False
+
 if __name__=="__main__":
   app.run(debug=True)
