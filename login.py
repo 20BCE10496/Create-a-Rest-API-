24eca5cb6506370a34cefcef7be0aea1d273ada0
20BCE10496 <amishagoyal2805@gmail.com>
2023-01-26 22:53:08 +0530

    update

--- login.py
+++ login.py
@@ -2,6 +2,7 @@ from flask import Flask, request,render_template, redirect
 import os
 import sqlite3
 
+# to set path  and location of file
 currentlocation = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 
