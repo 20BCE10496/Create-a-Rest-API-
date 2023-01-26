c12afc7b67fdf7f010e73423343022d3f405e466
20BCE10496 <amishagoyal2805@gmail.com>
2023-01-26 20:27:22 +0530

    update

--- login.py
+++ login.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, request,render_template, redirect
+import os
+import sqlite3
+
+currentlocation = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+if __name__=="__main__":
+  app.run(debug=True)
 
    
