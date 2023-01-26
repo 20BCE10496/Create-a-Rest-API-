d28cc7ceb2f0dd644f8e11e8a2ae49386ed105be
20BCE10496 <amishagoyal2805@gmail.com>
2023-01-27 01:32:33 +0530

    Update

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
@@ -6,21 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("login.html")
-database={'Amisha':'123','Anju':'aac','Anil':'asdsf'}
+    return render_template('login.html')
+database = {'Amisha': '123', 'Anju': 'aac', 'Anil': 'asdsf'}
 
-@app.route('/form_login',methods=['POST','GET'])
+
+@app.route('/form_login', methods=['POST', 'GET'])
 def login():
-    name1=request.form['username']
-    pwd=request.form['password']
+    name1 = request.form['username']
+    pwd = request.form['password']
+
     if name1 not in database:
-	    return render_template('login.html',info='Invalid User') 
-        
-    else :
-        if database[name1]!=pwd:
-            return render_template('login.html',info='Invalid Password')
+        return render_template('login.html', info='Invalid User')
+    else:
+        if database[name1] != pwd:
+            return render_template('login.html', info='Invalid Password')
         else:
-	         return render_template('home.html',name=name1)
+            return render_template('home.html', name=name1)
 
 if __name__ == '__main__':
     app.run()
